120466de43540025c7afdebfef6fab0bcded5183
Sergey V. Beduev <beduev@gmail.com>
2025-08-05 07:30:19 +0200

    Add Open Graph and Twitter meta tags to view-secret page

--- templates.go
+++ templates.go
@@ -3,6 +3,7 @@ package main
 import (
 	"html/template"
 	"net/http"
+	"strings"
 )
 
 
@@ -12,6 +13,25 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func viewSecretHandler(w http.ResponseWriter, r *http.Request) {
+	// Build the base URL for Open Graph meta tags
+	scheme := "https"
+	if r.Header.Get("X-Forwarded-Proto") != "" {
+		scheme = r.Header.Get("X-Forwarded-Proto")
+	} else if r.TLS == nil && !strings.Contains(r.Host, "localhost") && !strings.Contains(r.Host, "127.0.0.1") {
+		scheme = "http"
+	}
+	
+	baseURL := scheme + "://" + r.Host
+	requestURL := baseURL + r.URL.Path
+	
+	data := struct {
+		BaseURL    string
+		RequestURL string
+	}{
+		BaseURL:    baseURL,
+		RequestURL: requestURL,
+	}
+	
 	tmpl := template.Must(template.ParseFS(templatesFS, "templates/view-secret.html"))
-	tmpl.Execute(w, nil)
+	tmpl.Execute(w, data)
 }
